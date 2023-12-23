cf7efea4b0c16914edfbf16c31db0ff50c89ed9d
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2023-12-23 08:32:00 +0530

    update validator util

---
+++ src/util/validator.ts
@@ -0,0 +1,14 @@
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const sriLankanMobileRegex = /^7|0|(?:\ +94)[0-9]{9,10}$/;
+
+export function validateEmail(email: string): boolean {
+    return emailRegex.test(email);
+}
+
+export function validateLKMobileNumbers(mobile: string): boolean {
+    return sriLankanMobileRegex.test(mobile);
+}
+
+export function validatePassword(password: string): boolean {
+    return password>=8;
+}
